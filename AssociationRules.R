45e7641699f3ca856d5d5e2ab85d3dff2d121d7c
Thomson Kneeland <thomsonkneeland@yahoo.com>
2018-11-09 18:37:31 -0500

    Updates for Association Mining Graphs

--- AssociationRules.R
+++ AssociationRules.R
@@ -4,6 +4,7 @@ library(ggplot2)  # for graphics
 library(scales) # pretty_breaks , etc
 library(gtools)  ## for testing validity
 library(dplyr) # for manipulating dataframe
+library(RColorBrewer)  # for added colors
 
 setwd("C:/Users/tknee/Desktop/TelecomStockSelector") ## set working directory
 
@@ -15,71 +16,108 @@ quarters <-read.transactions("VerizonCat.csv",format="basket",sep=",",cols=c(1))
 inspect(quarters)
 
 #calculate Support and see frequent items
-frequentItems <- eclat(quarters,parameter=list(supp=.20,maxlen=15))
-inspect(frequentItems)
-itemFrequencyPlot(items(frequentItems),topN=10,type="relative",main="Item Frequency")  # or "relative" type
+# this is a different package.  USe below arulesViz!!!
+#frequentItems <- eclat(quarters,parameter=list(supp=.15,maxlen=15))
+#inspect(frequentItems)
+#itemFrequencyPlot(items(frequentItems),topN=10,type="absolute",main="Item Frequency, Support=.15",cex.names=.9,col=brewer.pal(8,'Pastel2'))  # or "relative" type
 
-# Get rules for dataset
-rules <- apriori(quarters,parameter = list(supp=0.10,conf=0.3,target="rules"))
-# sort rules for high confidence
+# Get rules for all of dataset
+rules <- apriori(quarters,parameter = list(supp=0.10,conf=0.5,target="rules"))
+# High Confidence Rules
 rules_conf <- sort(rules, by="confidence",decreasing=TRUE)
-inspect(rules_conf)
-inspect(head(rules_conf,10)) # add ,10 at end to get more rules
-plot(rules_conf)
-subrules2 <- head(rules_conf, n = 10, by = "confidence")
-plot(subrules2, method = "graph")
-plot(subrules2, method = "matrix3D")
-plot(subrules2, method = "mosaic") # mosaic can only visualize one rule
-plot(subrules2, method = "grouped")  # Use this to figure out rules
+inspect(head(rules_conf,10)) 
+# High Lift Rules
+rules_lift <- sort(rules, by="lift",decreasing=TRUE)
+#subrules2 <- head(rules_lift)
+inspect(head(rules_lift,10))
+#plot(rules_lift, measure=c("support","lift"),shading="confidence",jitter=0)
+# plot(subrules2, method = "two-key plot")
+rules_lift1=head(rules_lift,10)
+plot(rules_lift1, method = "graph") #YES
+plot(rules_lift, method = "matrix3D")  # Possible
+plot(rules_lift, method = "grouped",gp_labels = gpar(col="dark blue",cex=1,fontface="bold"))  # Use this to figure out rules  
+plot(rules_lift, method = "paracoord") 
+inspectDT(rules_lift) # YES USE THIS
 
+# SINGLE RULE VISUALIZATIONA
+plot(rules_lift, method = "mosaic") # mosaic can only visualize one rule  YES THIS CONFIRMS FINDINGS
+
+# Find High Confidence Rules
+inspectDT(rules_conf) # Use this graphic
 
 # Find High Lift Rules
 rules_lift <- sort(rules, by="lift", decreasing=TRUE)
-inspect(head(rules_lift))
-# find what influenced Specific Price Change:  NO RULES COMING UP! MUST BE GETTING ELIMINATED
-# I lowered confidence. NOt giving us anything workable since too few rows
-price_rulesEPos <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Extremely Positive"),control=list(verbose=F))
-price_rules_conf_EPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)  
-inspect(head(price_rules_conf_EPos))   # NOt displaying
-price_rules_lift_EPos <- sort(price_rulesEPos, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift_EPos))
+inspectDT(rules_lift)  #  Use this
 
-price_rulesVPos <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Very Positive"),control=list(verbose=F))
-price_rules_confVPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)
+# FIND ASSOCIATION RULES FOR SPECIFIC PRICE OUTCOMES
+# Since there are only 47 rows, these have to have very low support!!!!!  (since 6 categories)
+
+#EXTREMELY POSITIVE
+price_rulesEPos <- apriori(data=quarters,parameter=list(supp=.05,conf=.3),appearance=list(default="lhs",rhs="Price=Extremely Positive"),control=list(verbose=F))
+price_rules_confEPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)  
+inspect(head(price_rules_confEPos))   # If doesnt display, lower support or confidence
+price_rules_liftEPos <- sort(price_rulesEPos, by="lift", decreasing=TRUE)
+inspect(head(price_rules_liftEPos))
+#visualize top rule
+r1 <- sample(price_rules_confEPos,1)
+inspect(r1)
+plot(r1, method = "mosaic",data=quarters,control=list(main='Price'))  #Title not working
+
+# VERY POSITIVE
+price_rulesVPos <- apriori(data=quarters,parameter=list(supp=.05,conf=.5),appearance=list(default="lhs",rhs="Price=Very Positive"),control=list(verbose=F))
+price_rules_confVPos <- sort(price_rulesVPos, by="confidence",decreasing=TRUE)
 inspect(head(price_rules_confVPos))
-price_rules_lift_VPos <- sort(price_rulesVPos, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift_VPos))
+r2 <- sample(price_confVPos,1)
+inspect(r2)
+plot(r2, method = "mosaic",data=quarters,col="blue",col=grey.colors) # mosaic can only visualize one rule
 
-# NOT DONE
-price_rulesPos <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Positive"),control=list(verbose=F))
-price_rules_conf_EPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)  
-inspect(head(price_rules_conf_EPos))   # NOt displaying
-price_rules_lift_EPos <- sort(price_rulesEPos, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift_EPos))
+# POSITIVE   4 RULES FOUND
+price_rulesPos <- apriori(data=quarters,parameter=list(supp=.05,conf=.6),appearance=list(default="lhs",rhs="Price=Positive"),control=list(verbose=F))
+price_rules_confPos <- sort(price_rulesPos, by="confidence",decreasing=TRUE)  
+inspect(head(price_rules_confPos))   # NOt displaying
+price_rules_liftPos <- sort(price_rulesPos, by="lift", decreasing=TRUE)
+inspect(head(price_rules_liftPos))
+#visualize top rule
+r3 <- sample(price_rules_confPos,1)
+inspect(r3)
+plot(r3, method = "mosaic",data=quarters,control=list(main='Price'))  #Title not working
 
-# NOTE DONE
-price_rulesNeg <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Negative"),control=list(verbose=F))
-price_rules_conf_EPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)  
-inspect(head(price_rules_conf_EPos))   # NOt displaying
-price_rules_lift_EPos <- sort(price_rulesEPos, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift_EPos))
+# NEGATIVE  # 1 RULE FOUND NOT GREAT
+price_rulesNeg <- apriori(data=quarters,parameter=list(supp=.05,conf=.5),appearance=list(default="lhs",rhs="Price=Negative"),control=list(verbose=F))
+price_rules_confNeg <- sort(price_rulesNeg, by="confidence",decreasing=TRUE)  
+inspect(head(price_rules_confNeg))   # NOt displaying
+price_rules_liftNeg <- sort(price_rulesNeg, by="lift", decreasing=TRUE)
+inspect(head(price_rules_liftNeg))
+#visualize top rule
+r4 <- sample(price_rules_confNeg,1)
+inspect(r4)
+plot(r4, method = "mosaic",data=quarters,control=list(main='Price'))  #Title not working
 
-# NOT DONE
-price_rulesVNeg <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Very Negative"),control=list(verbose=F))
-price_rules_conf_EPos <- sort(price_rulesEPos, by="confidence",decreasing=TRUE)  
-inspect(head(price_rules_conf_EPos))   # NOt displaying
-price_rules_lift_EPos <- sort(price_rulesEPos, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift_EPos))
+# VERY NEGATIVE
+price_rulesVNeg <- apriori(data=quarters,parameter=list(supp=.05,conf=.5),appearance=list(default="lhs",rhs="Price=Very Negative"),control=list(verbose=F))
+price_rules_confVNeg <- sort(price_rulesVNeg, by="confidence",decreasing=TRUE)  
+inspect(head(price_rules_confVNeg))   # NOt displaying
+price_rules_liftVNeg <- sort(price_rulesVNeg, by="lift", decreasing=TRUE)
+inspect(head(price_rules_liftVNeg))
+#visualize top rule
+r5 <- sample(price_rules_confVNeg,1)
+inspect(r5)
+plot(r5, method = "mosaic",data=quarters,control=list(main='Price'))  #Title not working
 
-#NOT DONE
-price_rulesENeg <- apriori(data=quarters,parameter=list(supp=.10,conf=.3),appearance=list(default="lhs",rhs="Price=Extremely Negative"),control=list(verbose=F))
-price_rules_conf <- sort(price_rules, by="confidence",decreasing=TRUE)
-inspect(head(price_rules_conf))
-price_rules_lift <- sort(price_rules, by="lift", decreasing=TRUE)
-inspect(head(price_rules_lift))
+#EXTREMELY NEGATIVE  # NOTHING STANDS OUT, ALL LOW SUPPORT (1 count each)
+price_rulesENeg <- apriori(data=quarters,parameter=list(supp=.01,conf=.3),appearance=list(default="lhs",rhs="Price=Extremely Negative"),control=list(verbose=F))
+price_rules_confENeg <- sort(price_rulesENeg, by="confidence",decreasing=TRUE)
+inspect(head(price_rules_confENeg))
+price_rules_liftENeg <- sort(price_rulesENeg, by="lift", decreasing=TRUE)
+inspect(head(price_rules_liftENeg))
+#visualize top rule
+r6 <- sample(price_rules_confENeg,1)
+inspect(r6)
+plot(r6, method = "mosaic",data=quarters,control=list(main='Price'))  #Title not working
+
 
 # Remove redundant rules  NOT HELPING AT ALL ELMINATES ALL RULES
-subsetRules <- which(colSums(is.subset(price_rules_lift,price_rules_lift))>1)
-length(subsetRules)
-price_rules_lift <- price_rules_lift[-subsetRules] 
-inspect(head(price_rules_lift))
+subsetRulesPos <- which(colSums(is.subset(price_rules_liftPos,price_rules_liftPos))>1)
+length(subsetRulesPos)
+price_rules_liftPos <- price_rules_liftPos[-subsetRules] 
+inspect(head(price_rules_liftPos))
